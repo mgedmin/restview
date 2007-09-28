89473b6a354c4c24d09bde9b88b36bf603f6aa5c
mg <>
2007-09-28 21:40:30 +0000

    Make tests work again.

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import unittest
 import doctest
 
 def test_suite():
-    return doctest.DocTestSuite('restviewhttp')
+    return doctest.DocTestSuite('restview.restviewhttp')
 
 if __name__ == '__main__':
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
     unittest.main(defaultTest='test_suite')
