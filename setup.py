075859cf9880e6ed17056d087b410993f7974865
mg <>
2008-07-26 10:23:23 +0000

    Bump version to 1.0.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(filename):
 
 setup(
     name='restview',
-    version='1.0.1dev',
+    version='1.0.1',
     author='Marius Gedminas',
     author_email='marius@gedmin.as',
     url='http://mg.pov.lt/restview/',
